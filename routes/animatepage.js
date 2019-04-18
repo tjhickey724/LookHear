168f5d9430cb43b3994637e71b57632ad22e2ab6
ejydavis <ejesyang@gmail.com>
2019-04-17 22:51:07 -0400

    animate page now takes media from piece directories

--- routes/animatepage.js
+++ routes/animatepage.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const Piece = require('../models/pieces.model.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('animatepage', { title: 'The Animation Page' });
+});
+
+router.get('/:pieceId', function(req, res, next) {
+  let pieceId = req.url;
+  pieceId = pieceId.slice(1);
+  //let result = Piece('parts').find( { _id: pieceId });
+  Piece
+  .find({
+    _id: pieceId
+  })
+  .then(doc => {
+    let currPiece = doc;
+    let currTitle = currPiece[0].title;
+    let currOwner = currPiece[0].owner;
+    let currParts = currPiece[0].parts;
+    let currPieceId = currPiece[0].id;
+    console.log(currTitle);
+    res.render('animatepagemodule', { pieceTitle : currTitle, pieceOwner : currOwner, pieceParts : currParts, pieceId : currPieceId });
+  })
+  .catch(err => {
+    console.error(err)
+  })
 });
 
 module.exports = router;
